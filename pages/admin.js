da6b68a3620579e707e9f6538fe0b2753955e534
sametalagoz019-svg <sametalagoz019@gmail.com>
2026-03-25 22:47:34 +0300

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,3 +1,18 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Admin() {
-  return <h1>ADMIN PANEL</h1>;
+  const router = useRouter();
+
+  useEffect(() => {
+    if (localStorage.getItem("auth") !== "ok") {
+      router.push("/login");
+    }
+  }, []);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Admin Panel</h1>
+    </div>
+  );
 }
